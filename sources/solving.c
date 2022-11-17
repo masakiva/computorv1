7731d9d54acd642ee2bf83ca7c4a710d3cd33454
masaki <git@kittens.army>
2022-11-17 16:26:48 +0900

    merge terms of the same degree

--- sources/solving.c
+++ sources/solving.c
@@ -6,7 +6,7 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 18:54:17 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/17 15:32:20 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/11/17 16:26:44 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,39 +41,7 @@ void	print_terms(t_equation* equation)
 	}
 }
 
-void	sort_terms(t_list* terms_list)
-{
-	t_list*	cur_link;
-	t_term*	cur_term;
-	t_list*	cur_link_2;
-	t_term*	cur_term_2;
-	int		swap_exp;
-	double	swap_param;
-
-	cur_link = terms_list;
-	while (cur_link != NULL)
-	{
-		cur_term = cur_link->content;
-		cur_link_2 = cur_link;
-		while (cur_link_2 != NULL)
-		{
-			cur_term_2 = cur_link_2->content;
-			if (cur_term->exponent > cur_term_2->exponent)
-			{
-				swap_exp = cur_term->exponent;
-				cur_term->exponent = cur_term_2->exponent;
-				cur_term_2->exponent = swap_exp;
-				swap_param = cur_term->parameter;
-				cur_term->parameter = cur_term_2->parameter;
-				cur_term_2->parameter = swap_param;
-			}
-			cur_link_2 = cur_link_2->next;
-		}
-		cur_link = cur_link->next;
-	}
-}
-
-void	reduce_eq_form(t_equation* equation)
+void	merge_right_to_left(t_equation* equation)
 {
 	t_list*	cur_link_r;
 	t_list*	cur_link_l;
@@ -111,6 +79,89 @@ void	reduce_eq_form(t_equation* equation)
 		ft_lstdelone(cur_link_r, free_content);
 		cur_link_r = equation->right_terms;
 	}
+}
+
+void	merge_left_terms(t_list* terms_list)
+{
+	t_list*	cur_link;
+	t_term*	cur_term;
+	t_list*	cur_link_2;
+	t_list*	cur_link_2_prev;
+	t_term*	cur_term_2;
+
+	cur_link = terms_list;
+	while (cur_link != NULL)
+	{
+		cur_term = cur_link->content;
+		cur_link_2 = cur_link->next;
+		cur_link_2_prev = cur_link;
+		while (cur_link_2 != NULL)
+		{
+			cur_term_2 = cur_link_2->content;
+			if (cur_term->exponent == cur_term_2->exponent)
+			{
+				cur_term->parameter += cur_term_2->parameter;
+				cur_link_2_prev->next = cur_link_2->next;
+				ft_lstdelone(cur_link_2, free_content);
+			}
+			cur_link_2_prev = cur_link_2;
+			cur_link_2 = cur_link_2->next;
+		}
+		cur_link = cur_link->next;
+	}
+}
+
+void	remove_null_terms(t_list* terms_list)
+{
+	t_list*	cur_link;
+	t_term*	cur_term;
+	//t_list*	cur_link_prev;
+
+	cur_link = terms_list;
+	while (cur_link != NULL)
+	{
+		cur_term = cur_link->content;
+		cur_link = cur_link->next;
+	}
+}
+
+void	sort_terms(t_list* terms_list)
+{
+	t_list*	cur_link;
+	t_term*	cur_term;
+	t_list*	cur_link_2;
+	t_term*	cur_term_2;
+	int		swap_exp;
+	double	swap_param;
+
+	cur_link = terms_list;
+	while (cur_link != NULL)
+	{
+		cur_term = cur_link->content;
+		cur_link_2 = cur_link->next;
+		while (cur_link_2 != NULL)
+		{
+			cur_term_2 = cur_link_2->content;
+			if (cur_term->exponent > cur_term_2->exponent)
+			{
+				swap_exp = cur_term->exponent;
+				cur_term->exponent = cur_term_2->exponent;
+				cur_term_2->exponent = swap_exp;
+				swap_param = cur_term->parameter;
+				cur_term->parameter = cur_term_2->parameter;
+				cur_term_2->parameter = swap_param;
+			}
+			cur_link_2 = cur_link_2->next;
+		}
+		cur_link = cur_link->next;
+	}
+}
+
+void	reduce_eq_form(t_equation* equation)
+{
+	merge_right_to_left(equation);
+	merge_left_terms(equation->left_terms);
+	//remove_null_terms(equation->left_terms);
 	sort_terms(equation->left_terms);
 }
 
