2506e4d883e055e2b8f0dbfd83027cfe4dc6b079
masaki <git@kittens.army>
2022-12-02 18:06:54 +0900

    fix ft_atof_double to use double limits

--- libft/custom/ft_atof.c
+++ libft/custom/ft_atof.c
@@ -6,11 +6,36 @@
 /*   By: mvidal-a <mvidal-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/16 16:33:52 by mvidal-a          #+#    #+#             */
-/*   Updated: 2022/11/16 17:17:53 by mvidal-a         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:02:47 by mvidal-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int8_t	atof_int_part(const char* str, double* nb)
+{
+	int8_t	sign_or_error;
+
+	sign_or_error = 1;
+	str = skip_spaces((char*)str);
+	if (*str == '-')
+	{
+		sign_or_error = -1;
+		str++;
+	}
+	else if (*str == '+')
+		str++;
+	*nb = 0.0;
+	if (*str < '0' || *str > '9')
+		sign_or_error = FAILURE;
+	while (*str >= '0' && *str <= '9')
+	{
+		*nb = *nb * 10 + *str - '0';
+		str++;
+	}
+	*nb *= sign_or_error;
+	return (sign_or_error);
+}
 
 static double	atof_decimal_part(char* str)
 {
@@ -35,15 +60,13 @@ static double	atof_decimal_part(char* str)
 
 double		ft_atof(const char* str)
 {
-	double	nb_f;
-	int		nb_i;
+	double	nb;
 	int8_t	sign_or_error;
 
-	sign_or_error = ft_atoi_sign(str, &nb_i);
-	nb_f = (double)nb_i;
+	sign_or_error = atof_int_part(str, &nb);
 	if (sign_or_error == 1)
-		nb_f += atof_decimal_part(skip_int((char*)str));
+		nb += atof_decimal_part(skip_int((char*)str));
 	else if (sign_or_error == -1)
-		nb_f -= atof_decimal_part(skip_int((char*)str));
-	return (nb_f);
+		nb -= atof_decimal_part(skip_int((char*)str));
+	return (nb);
 }
